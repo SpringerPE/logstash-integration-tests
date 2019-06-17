c547c842c5ea9a91d8abe05b35cde176effe9ebb
antoniocascais <antonio.cascais@springer.com>
2019-06-17 15:54:46 +0200

    Added scripts to update files and run tests
    The python script will create/change the necessary files.
    It will read data from `data/input.json` and process the information,
    creating the `.input.log` file (which will be the source of events to
    lgostash) and updating the `conf/output.conf` file with the fields that
    we want to test and the `scripts/run_tests.sh` file with the assertions.

---
+++ scripts/run_tests.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# This script will load the logstash output file and compare the values of the fields that are defined there with the
+# expectations.
+# Returns `exit 1` in case of failure.
+
+INPUT="/data/logstash.csv"
+
+# Credits to https://stackoverflow.com/a/24597941/10985108
+function error_exit {
+    echo "$1" >&2   ## Send message to stderr. Exclude >&2 if you don't want it that way.
+    exit "${2:-1}"  ## Return a code specified by $2 or 1 by default.
+}
+
+run_tests ()
+{
+    declare -a expectations
+    expectations=()
+
+    while IFS= read -r line
+    do
+      IFS=';' read -ra arr <<< "$line"
+      for i in "${!arr[@]}"; do
+         if [ "${arr[$i]}" == "${expectations[$i]}" ]
+         then
+           printf "TEST $i PASSED; \n"
+         else
+           error_exit "TEST FAILED: got | ${arr[$i]} |; expected | ${expectations[$i]} |"
+         fi
+      done
+    done < "$INPUT"
+}
+
+check_if_file_exists ()
+{
+    max_attempts=90
+    attempts=1
+
+    while ! test -f $INPUT && [[ $attempts -lt $max_attempts ]]
+    do
+	    sleep 1
+	    ((attempts++))
+    done
+    
+    if [[ $attempts -eq $max_attempts ]]
+    then
+	    error_exit "FAILED: $input does not exist" 
+    fi
+}
+
+check_if_file_exists
+run_tests
---
+++ scripts/wait_for_logstash.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# This script will wait for logstash to be started (will try to connect via TCP) and, in case of success, will run the
+# tests.
+# Exit with error in case logstash doesn't start on time.
+
+IP=192.168.200.2
+PORT=5514
+
+MAX_ATTEMPTS=90
+ATTEMPTS=1
+INTERVAL=1
+
+$(rm -rf /data/logstash.csv)
+
+$(nc -z $IP $PORT)
+status=$?
+
+while [[ $status -gt 0 && $ATTEMPTS -lt $MAX_ATTEMPTS ]]
+do
+  $(nc -z $IP $PORT)
+  status=$?
+  sleep $INTERVAL
+  ((ATTEMPTS++))
+done
+
+if [[ $ATTEMPTS -lt $MAX_ATTEMPTS ]]
+then
+  echo $(/tmp/run_tests.sh)
+else	
+  echo "FAILURE: logstash start timed out"
+  exit 1
+fi
